0308a93e2907c284dc8e18008e206180636319a8
Vidroha Debroy <vdebroy.github@gmail.com>
2022-03-29 11:28:50 -0500

    Update webserver.js
    Adding a variable to demo secrets.

--- docker-samples/simple-webserver/webserver.js
+++ docker-samples/simple-webserver/webserver.js
@@ -3,6 +3,7 @@ var app     = express();
 
 var port = process.env.PORT || 8080
 var ENV_TEST = process.env.ENV_TEST || "ENV_TEST is unset"
+var SECRET_TEST = process.env.SECRET_TEST || "SECRET_TEST is unset"
 
 app.get('/', function(req, res) {
     res.sendfile('index.html');
@@ -13,3 +14,4 @@ app.listen(port);
 console.log('Start Webserver on port:' + port);
 
 console.log('Value of ENV_TEST:' + ENV_TEST);
+console.log('Value of SECRET_TEST:' + SECRET_TEST);
