f1e1d68a1fa4ab7c24446bbc8297bd2e0fe2f480
Vidroha Debroy <vdebroy.github@gmail.com>
2022-03-29 10:10:56 -0500

    Updating webserver to use PORT environment variable with default.

--- docker-samples/simple-webserver/webserver.js
+++ docker-samples/simple-webserver/webserver.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app     = express();
 
+var port = process.env.PORT || 8080
+
 app.get('/', function(req, res) {
     res.sendfile('index.html');
 });
 
-app.listen(80);
+app.listen(port);
 
-console.log('Start Webserver');
+console.log('Start Webserver on port:' + port);
